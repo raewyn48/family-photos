733165cdb600d6fcd94154f973bd80954baaf948
Raewyn <rae88.falkor.gen.nz>
2016-05-09 14:04:29 +1200

    List file names.

---
+++ js/app.js
@@ -0,0 +1,35 @@
+var Photo = function(data) {
+  
+  this.fileName = ko.observable(data.FileName);
+}
+
+var ViewModel = function() {
+  var self = this;
+  
+  this.photoList = ko.observableArray([]);
+
+  $.getJSON("/photo_api/json/IMG_7870.json", function(data) {
+    data.forEach(function(photoData) {
+      self.photoList.push(new Photo(photoData));
+    });
+  });
+  /*
+  this.catList = ko.observableArray([]);
+  
+  initialCats.forEach(function(catItem) {
+    self.catList.push(new Cat(catItem));
+  });
+
+  this.currentCat = ko.observable( this.catList()[0] );
+
+  this.incrementCounter = function() {
+    self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+  };
+  
+  this.changeCat = function(newCat) {
+    self.currentCat(newCat);
+  };
+*/
+};
+
+ko.applyBindings(new ViewModel());
